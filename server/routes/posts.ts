9b782d427c2be80d747c624e35445da02e7e9407
IOAyman <theycallmethedr@gmail.com>
2016-06-28 03:05:43 +0100

    implemented /posts endpoint

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -2,7 +2,43 @@
 
 import {Router} from 'express'
 import {normalize, join} from 'path'
+import {readFileSync} from 'fs'
 
 let router = Router()
+
+// Post contract
+interface Post {
+    id: Number
+    author: string
+    date: string
+    text: string
+    video: string
+    audio: string
+}
+
+// get static data
+let f:string = normalize(join(process.cwd(), './raw/posts.json'))
+let posts:Post[] = JSON.parse(readFileSync(f, 'utf8'))
+
+// get all posts
+router.get('/', (req,res)=>{
+    res.json(posts)
+})
+
+// get post by id
+router.get('/:id', (req,res, next)=>{
+    for (let post of posts)
+        if (post.id == parseInt(req.params.id, 10))
+            res.json(post)
+    next(new Error('Cannot find post with id:' + req.params.id))
+})
+
+router.use((err:Error, req, res, next) => {
+    if (err){
+        console.error(err.message)
+        res.status(404).json({msg: err.message})
+    } 
+    else next()
+})
 
 export = router
