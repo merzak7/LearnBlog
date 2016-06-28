4ce41cb9350dd441f839425fb0123721697a90c1
IOAyman <theycallmethedr@gmail.com>
2016-06-28 02:25:26 +0100

    init posts route

--- server/routes/init.ts
+++ server/routes/init.ts
@@ -2,11 +2,16 @@
 
 import {Router} from 'express'
 
-
 let router = Router()
 
+// root
 router.get('/', (req,res)=>{
     res.end('Hello, World!\n')
 })
+
+// posts
+import * as posts from './posts'
+router.use('/posts', posts)
+
 
 export = router
---
+++ server/routes/posts.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/tsd.d.ts"/>
+
+import {Router} from 'express'
+import {normalize, join} from 'path'
+
+let router = Router()
+
+export = router
