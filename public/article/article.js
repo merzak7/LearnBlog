1bf112d6fbc8301c74c7fba9924e5e8d37e75f33
yahia <yahia077@hotmail.com>
2016-06-29 16:03:42 +0100

    added method add article & modifiy articles.html

--- public/article/article.js
+++ public/article/article.js
@@ -5,23 +5,34 @@ article.service('ArticleServices',function(){
 		var article={};
 		this.getArticle=function(){
 			return article;
-		}
+		};
 		this.setArticle=function(art){
 			article=art;
-		}
+		};
 });
 
 article.controller('ArticleCtrl',function($scope,$location,$resource,ArticleServices){
-	
-	//$scope.article="first article";
 
 	var res=$resource("http://localhost:8000/posts");//:id",{id:'@id'},{action:{method :"GET"}});
-	  $scope.articles=res.query();
+	$scope.articles=res.query();
 
 	 $scope.getArticle=function(article){
 	 	ArticleServices.setArticle(article);
 	 	$location.path('/article_detail');
-	 } 
+	 };
+
+	 $scope.addArticle=function(){
+	 	var article={
+	 				 "title":$scope.title,
+	 				 "author":"yahia",
+	 				 "text":$scope.text,
+	 				 "video":$scope.video,
+	 				 "audio":$scope.audio
+	 				};
+	 	res.save(article);
+	 	$scope.articles=res.query();
+	 	
+	 };
 });
 
 article.controller('Article_detailCtrl',function($scope,ArticleServices){
