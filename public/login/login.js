f35a04bd80759eccb33f846b751800adb13bd411
yahia <yahia077@hotmail.com>
2016-06-30 17:24:23 +0100

    added link to icons in header.html & modified home.js & added login service & change login controller & mdified login.html

--- public/login/login.js
+++ public/login/login.js
@@ -1,11 +1,24 @@
 
 // services
+app.service('LoginServices',function(){
+	var isLogged=false;
+
+	this.submit=function(email,password){
+		if(email=="admin" && password=="admin"){
+			logged=true;
+			return logged;
+		}
+	};
+});
 // controllers
-app.controller('LoginCtrl',function($scope){
+app.controller('LoginCtrl',function($scope,$location,LoginServices){
 	$scope.page_title="Login";
 	$scope.submit=function(){
-		$scope.user.email="logged";
-	}
+		var isLogged=LoginServices.submit($scope.user.email,$scope.user.password);
+		if(isLogged){
+			$location.path('/article');
+		}
+	};
 	
 });
 //directives
