4ee4b560581b65364b82814786178ab50b852971
yahia <yahia077@hotmail.com>
2016-06-30 18:44:42 +0100

    update login js and html file

--- public/login/login.js
+++ public/login/login.js
@@ -3,18 +3,23 @@
 app.service('LoginServices',function(){
 	var isLogged=false;
 
-	this.submit=function(email,password){
-		if(email=="admin" && password=="admin"){
-			logged=true;
-			return logged;
+	this.submit=function(user){
+		var user1={"email":"admin","password":"admin"}
+		if(angular.equals(user,user1)){
+			isLogged=true;
 		}
+	};
+
+	this.isLogged=function(){
+		return isLogged;
 	};
 });
 // controllers
 app.controller('LoginCtrl',function($scope,$location,LoginServices){
 	$scope.page_title="Login";
-	$scope.submit=function(){
-		var isLogged=LoginServices.submit($scope.user.email,$scope.user.password);
+	$scope.submit=function(user){
+		LoginServices.submit(user);
+		var isLogged=LoginServices.isLogged();
 		if(isLogged){
 			$location.path('/article');
 		}
