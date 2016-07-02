6675c96d40aba61afba04584d41419bff2cdde9b
IOAyman <theycallmethedr@gmail.com>
2016-07-02 16:55:20 +0100

    added /users endpoint

--- server/routes/init.ts
+++ server/routes/init.ts
@@ -2,12 +2,14 @@
 
 import {Router} from 'express'
 import * as posts from './posts'
+import * as users from './users'
 
 let router = Router()
 
 
 // posts
 router.use('/posts', posts)
+router.use('/users', users)
 
 
 export = router
