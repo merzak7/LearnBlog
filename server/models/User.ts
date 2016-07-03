b0486bbd212bc916ada6fbd33894ba1e622497ee
IOAyman <theycallmethedr@gmail.com>
2016-07-03 18:10:14 +0100

    bug fixes

--- server/models/User.ts
+++ server/models/User.ts
@@ -6,15 +6,15 @@ import {readFile, writeFile} from 'fs'
 
 // user contract
 export interface User {
-  id:number
+  id?:number
   username:string
   password:string
-  bio:string
-  url:string
-  role:ROLE
+  bio?:string
+  url?:string
+  role?:ROLE
 }
 
-type ROLE = 'admin' | 'user'
+export type ROLE = 'admin' | 'user'
 
 
 // database setup
@@ -30,20 +30,28 @@ readFile(f, 'utf8', (err, data)=>{
 
 
 export function findOneByUsername(username:string, callback:(err:Error, data:User)=>void) {
+  let u:User
   for (let user of users) {
+    console.log(user.username, 'vs', username)
     if (user.username === username)
-      callback(null, user)
-    else
-      callback(new Error('No such user as ' + username), null)
+      u = user
   }
+  u? callback(null, u) : callback(new Error('No such user as ' + username), null)
 }
 
-export function save(user:User) {
+export function save(user:User, callback:(err:Error) => void) {
   user.id = users.length
-  users.push(user)
-  writeFile(f, JSON.stringify(users), (err) => {
-    if(err) {
-      console.log(err.message)
-    }
+  let x = users.filter((u, index, users) => {
+    return u.username === user.username
   })
+  if (x.length > 0)
+    callback(new Error('User already exists!'))
+  else {
+    users.push(user)
+    writeFile(f, JSON.stringify(users), (err) => {
+      if(err) {
+        console.log(err.message)
+      }
+    })
+  }
 }
