a9efddbb03b5dedba91048a882d7a132a6712a64
IOAyman <theycallmethedr@gmail.com>
2016-07-04 16:33:45 +0100

    Redirect to home on successful login

--- public/login/login.js
+++ public/login/login.js
@@ -1,12 +1,16 @@
 
 // services
-app.service('LoginServices',function($resource){
+app.service('LoginServices',function($resource, $location){
 	var loginResponse={};
 
 	this.submit=function(user){
 		var login=$resource("http://localhost:8000/auth/login");
-		login.save({username:user.username,password:user.password});
-		console.log(user);
+		login.save({username:user.username,password:user.password},
+              function ok(){
+                $location.path('/home')
+              }, function notOk() {
+                // todo ~ make some notification..
+              });
 	};
 
 	this.loginResponse=function(){
@@ -19,6 +23,6 @@ app.controller('LoginCtrl',function($scope,$location,LoginServices){
 	$scope.submit=function(user){
 		LoginServices.submit(user);
 	};
-	
+
 });
 //directives
