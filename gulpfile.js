a76b34dbbcb38684c781201f005bf2a2fddf2455
Merzak <merzak17@gmail.com>
2016-07-05 18:09:48 +0100

    Update gulpfile (Fix reloading browser prblm)

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('watch:public', ['serve'], () => {
   browserSync.init({
     port: 9000,
     open: false,
-    proxy: 'http://localhost:8000',
+    server:{baseDir:'./public'},
     ui: {port: 9001}
   })
 gulp.watch(path.normalize(project.static))
